e6f39b81fb77567f474a01add182b1ce979f0d6a
wjdalla <danielafilipateodoro@gmail.com>
2025-06-02 17:05:13 +0100

    Novos Cursores - View_Projects

--- sketch.js
+++ sketch.js
@@ -1,38 +1,59 @@
-let custom_cursor_element, body_element;
 
+let cursor_element, cursor_w, cursor_h, view_project_element;
 
 function setup() 
 {
   noCanvas();
+
+  cursor_element = select ("#meu_cursor");
+  cursor_element.hide();
   
-  custom_cursor_element = select ("#meu_cursor");
-  custom_cursor_element.hide();
-
-  body_element = select ("body");
-
+  cursor_w = 100;
+  cursor_h = 100;
+  
+  view_project_element = select ("#view_project");
+  view_project_element.hide();
 }
 
 
 function draw() 
 {
-  custom_cursor_element.position (mouseX -35, mouseY -30, "fixed");
+  cursor_element.size  (cursor_w, cursor_h);
+  cursor_element.position (mouseX-cursor_w/2, mouseY-cursor_h/2, "fixed");
 }
 
-function hideCustomCursor()
-{
-    if (window.getComputedStyle(custom_cursor_element.elt)['display'] != "none") 
-    {
-        body_element.style('cursor', 'default !important');
-        custom_cursor_element.hide();
-    }
-}
-
-
+/*Controla o cursor quando este passa sobre links*/
+/*Faz com que o cursor apareça quando passa sobre link*/
 function showCustomCursor() 
 {
-    if (window.getComputedStyle(custom_cursor_element.elt)['display'] === "none") 
-    {
-        body_element.style('cursor', 'none !important');
-        custom_cursor_element.show();
-    }
+  cursor_w = 100;
+  cursor_h = 100;
+  cursor_element.show();
+}
+
+/*Controla o cursor quando este passa sobre links*/
+/*Faz com que o cursor desapareça quando passa sobre link*/
+function hideCustomCursor () 
+{
+  cursor_element.hide();
+}
+
+/*Controla o cursor quando este passa a galeria de trabalhos no index*/
+/*Faz com que o cursor aumente quando está sobre a imagem*/
+function growCustomCursor()
+{
+  cursor_w = 220;
+  cursor_h = 220;
+  cursor_element.show();
+  view_project_element.style("display", "flex");
+}
+
+/*Controla o cursor quando este passa a galeria de trabalhos no index*/
+/*Faz com que o cursor desapareça quando está sobre a imagem*/
+function shrinkCustomCursor() 
+{
+  cursor_w = 100;
+  cursor_h = 100;
+  view_project_element.hide();
+  cursor_element.hide();
 }
